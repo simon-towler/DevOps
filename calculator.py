7f8cd527953cfd4351d44355ff319ba71b657536
simon-towler <simon.towler@icloud.com>
2021-03-24 14:05:14 +0000

    Python calculator

---
+++ calculator.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+
+import sys, getopt, operator
+
+ops = {
+    '+' : operator.add,
+    '-' : operator.sub,
+    'x' : operator.mul,
+    '/' : operator.div
+}
+
+input_file = open("instructions_file.txt")
+
+running_total = 0
+
+for x in input_file:
+  args = x.split()
+  first_operand = args[2]
+  second_operand = args[3]
+  operator = ops.get(args[1])
+  running_total = running_total + operator(int(first_operand), int(second_operand))
+
+print running_total
